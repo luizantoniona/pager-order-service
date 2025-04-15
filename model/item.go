bbf4e7c81e616431a8fe05ecd557977ec505aa50
luizantoniona <luizantonio.na@hotmail.com>
2025-04-15 14:22:51 -0300

    feat: Creating item structure

---
+++ model/item.go
@@ -0,0 +1,9 @@
+package model
+
+type Item struct {
+	Code string `json:"code"`
+	Name string `json:"name"`
+	Description string `json:"description"`
+	Price float64 `json:"price"`
+	Quantity int `json:"quantity"`
+}
