0ea2a343cb06dbc4d5e84d3c2020ff17cea08026
luizantoniona <luizantonio.na@hotmail.com>
2025-04-15 14:23:30 -0300

    feat: creating basic order structure

---
+++ model/order.go
@@ -0,0 +1,13 @@
+package model
+
+import {
+	"time"
+}
+
+type Order struct {
+	ID string `json:"id"`
+	Customer Customer `json:"customer"`
+	Items []Item `json:"items"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
